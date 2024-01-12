0ff3ebcfeb9f25e4583a8d9ed914ed02564721f4
himanshu <h47290@gmail.com>
2024-01-12 23:33:32 +0530

    app updated

--- src/App.js
+++ src/App.js
@@ -3,48 +3,58 @@ import { useState } from "react";
 function App() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
-  const [editId, setEditId] = useState(0);
-  
+  const [editId, setEditId] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     if (editId) {
-      const editTodo= todos.find((i)=>i.id === editId);
-      const updatedTodos=todos.map((t)=>t.id===editTodo.id?(
-        t={id:t.id,todo}): {id:t.id, todo:t.todo}
-      );
+      const updatedTodos = todos.map((t) => {
+        if (t.id === editId) {
+          t.text = todo
+        }
+        return t;
+      })
       setTodos(updatedTodos)
-      setEditId(0)
-      setTodo("")
-      return;
+      setTodo('')
+      setEditId('')
+      return
     }
-    if (todo !== "") {
-      setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos]);
-      setTodo ("");
+
+    const newTodo = {
+      id: `${todo}-${Date.now()}`,
+      text: todo
     }
+    setTodos([newTodo, ...todos])
+    setTodo('')
   };
+
   const handleDelete =(id)=>{
 const delTodo = todos.filter((to)=>to.id !== id)
 setTodos([...delTodo]);
   };
   const handleEdit =(id)=>{
-    const editTodo = todos.find((i)=>i.id ===id);
-    setTodo(editTodo.todo);
+    const editTodo = todos.find((i)=>i.id === id);
+    setTodo(editTodo.text);
     setEditId(id);
+  }
+
+  const handleChange = (e) => {
+    setTodo(e.target.value)
   }
   return (
     <div className="App">
       <div className="container">
         <h1>Todo List App</h1>
         <form className="todoForm" onSubmit={handleSubmit}>
-          <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
-          <button type="submit">{editId? "Edit" : "Go" }</button>
+          <input name={'todo'} type="text" value={todo} onChange={handleChange} />
+          <button type="submit">{editId ? 'Edit' : 'Go'}</button>
         </form>
         <ul className="allTodos">
           {todos.map((t) => (
             <li className="signleTodo">
-              <span className="todoText" key={t.id}>{t.todo}</span>
-              <button onClick={()=>handleEdit(t.id)}>Edit</button>
+              <span className="todoText" key={t.id}>{t.text}</span>
+              <button onClick={()=> handleEdit(t.id)}>Edit</button>
               <button onClick={()=>handleDelete(t.id)}>Delete</button>
             </li>
           ))}
